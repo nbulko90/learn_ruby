6782a05f9ec3373949c041826abf70cff7a01dce
Nick Bulko <nbulko90@gmail.com>
2017-01-05 22:01:58 -0500

    Added solution for 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+def ftoc(temp)
+	new_temp = (temp-32)
+	new_temp = new_temp.to_f*5/9
+	return new_temp.to_f
+end
+
+def ctof(temp)
+	new_temp = temp.to_f*9/5
+	new_temp = new_temp+32
+	return new_temp
+end
