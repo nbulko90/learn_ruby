5bb27779fafa8a2497463ccc40f83572bd59b735
Nick Bulko <nbulko90@gmail.com>
2017-01-05 22:11:41 -0500

    Added solution for 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
-#write your code here
+def add (num1, num2)
+	answer = num1 + num2
+	return answer
+end
+
+def subtract(num1, num2)
+	answer = num1 - num2
+	return answer
+end
+
+def sum(array)
+	sum = 0
+	array.each { |a| sum+=a }
+	return sum
+end
+
+def multiply (num1, num2)
+	answer = num1 * num2
+	return answer
+end
