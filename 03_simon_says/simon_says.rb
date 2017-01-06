c9ce13ee53137cac9d692c91960b6a560d3c8b38
Nick Bulko <nbulko90@gmail.com>
2017-01-05 22:38:50 -0500

    Added solution for 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
-#write your code here
+def echo(string)
+	return string
+end
+
+def shout(string)
+	return string.upcase
+end
+
+def repeat(string)
+	return "#{string} " "#{string}"
+end
+
+def repeat(string, num_repeats=2)
+	return ([string] * num_repeats).join(" ")
+end
+
+def start_of_word(string, n)
+	return string[0,n]
+end
+
+def first_word(string)
+	return string.split.first
+end
+
+def titleize(string)
+  words = string.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
